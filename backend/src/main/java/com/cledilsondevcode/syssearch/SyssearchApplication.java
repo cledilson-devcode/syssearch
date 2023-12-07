a6fb852627c572e7e91d6d8c203e32e729c3dbda
cledilson <cledilsondevoffice@gmail.com>
2023-12-07 16:44:58 -0300

    feat: Criando projeto spring backend com o intuido de dividir entre back e front.

---
+++ backend/src/main/java/com/cledilsondevcode/syssearch/SyssearchApplication.java
@@ -0,0 +1,13 @@
+package com.cledilsondevcode.syssearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyssearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyssearchApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cledilsondevcode/syssearch/SyssearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cledilsondevcode.syssearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyssearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
